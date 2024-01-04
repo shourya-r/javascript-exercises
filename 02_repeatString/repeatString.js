ec1191e6db86c493a9fa6d8ca113e45a8f7032f5
shourya-r <rajputshourya7@gmail.com>
2024-01-04 13:00:17 +0530

    Solved Q1 and Q2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numRepeat) {
+    let ans = "";
+    if(numRepeat<0){
+        return "ERROR";
+    }
+    for(let i = 0; i<numRepeat; i++){
+        ans+=string;
+    }
+    return ans;
 };
 
 // Do not edit below this line
