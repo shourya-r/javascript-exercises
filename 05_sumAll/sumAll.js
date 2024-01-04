76555f937fecfe5d3117e7daf026d10f1b43fc5c
shourya-r <rajputshourya7@gmail.com>
2024-01-04 14:14:47 +0530

    Solved Q5 and Q6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof num1 !== "number" || typeof num2 != "number" || num1<0 || num2<0){
+        return "ERROR";
+    }
+    const lower_bound = Math.min(num1,num2);
+    const upper_bound = Math.max(num1,num2);
+    let sum = 0; 
+    for(let i = lower_bound; i<=upper_bound; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
